bb0d931af8138599efa53853853e1808eba06a3e
Ale Figueroa <yknx.4.b@gmail.com>
2016-10-14 18:36:25 -0500

    Create user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#roles' do
+    it 'should have user role by default' do
+      user = create(:user)
+      expect(user.has_role?(:user)).to be_truthy
+    end
+  end
 end
