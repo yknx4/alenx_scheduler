c31d1e00db3b70d25b92ee22211d1d07e2961ada
Ale Figueroa <yknx.4.b@gmail.com>
2016-10-14 18:21:03 -0500

    Add Roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,17 @@
 class User < ApplicationRecord
+
+  # After Create
+  after_create :assign_default_role
+
   rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable
+
+
+  def assign_default_role
+    self.add_role(:user) if self.roles.blank?
+  end
 end
