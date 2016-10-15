d99c4f0505f6ce5fde28c81b28b38be7a450cb3f
Ale Figueroa <yknx.4.b@gmail.com>
2016-10-14 23:41:31 -0500

    Add tenant reference to User factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :user do
+    tenant
     username              { Faker::Internet.user_name }
     email                 { Faker::Internet.email }
     password              "password"
