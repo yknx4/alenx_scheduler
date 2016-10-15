0bfc80254cda14a98757d6a375cb3fa61fe3d8f1
Ale Figueroa <yknx.4.b@gmail.com>
2016-10-14 23:43:21 -0500

    Always create an aparment with a tenant

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,4 +1,10 @@
 class Tenant < ApplicationRecord
   validates_presence_of :subdomain
   has_many :users
+  after_create :lease_apartment
+
+  private
+  def lease_apartment
+    Apartment::Tenant.create(self.subdomain)
+  end
 end
