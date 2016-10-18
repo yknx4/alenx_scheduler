3a9f23d6ee99197f21b033d89c63f6994e878946
Jorge Figueroa <yknx.4.b@gmail.com>
2016-10-18 13:01:13 -0500

    Populate schedule spec

--- spec/factories/schedules.rb
+++ spec/factories/schedules.rb
@@ -1,9 +1,35 @@
+def max(a, b)
+  a > b ? a : b
+end
+
+def min(a, b)
+  a < b ? a : b
+end
+
+def time_range
+  hours = [rand(24), rand(24)]
+  { "#{min(*hours).to_s.rjust(2, '0')}:00" => "#{max(*hours).to_s.rjust(2, '0')}:00" }
+end
+
 FactoryGirl.define do
   factory :schedule do
-    timezone "MyString"
-    holidays "MyString"
-    breaks ""
-    hours ""
-    schedulable nil
+    timezone { Faker::Address.time_zone }
+    holidays { (0..rand(10)).to_a.map { Faker::Date.between(Date.today, 1.years.from_now) } }
+    breaks do
+      {
+          Faker::Date.between(Date.today, 1.years.from_now)  => {'09:00' => '10:30', '16:00' => '16:30'},
+          Faker::Date.between(Date.today, 1.years.from_now)  => {'12:15' => '12:45', '13:30' => '14:00'},
+          Faker::Date.between(Date.today, 1.years.from_now)  => time_range
+      }
+    end
+    hours do
+      amount = rand(0)
+      amount = 1 if amount < 1
+      days = [:mon, :tue, :wed, :thu, :fri, :sat, :sun]
+      days.sample(amount).inject({}) do |hash, day|
+        hash[day] = time_range
+        hash
+      end
+    end
   end
 end
