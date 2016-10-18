e123c0eac30aa46fac47a4a4b705b108b59c1feb
Ale Figueroa <yknx.4.b@gmail.com>
2016-10-17 23:26:40 -0500

    Create HazBiz module

---
+++ app/models/concerns/has_biz_concern.rb
@@ -0,0 +1,38 @@
+module HasBizConcern
+  def holidays
+    super.map{|h| Date.parse(h)}
+  end
+
+  def hours
+    super.inject({}) do |new_hours, hour|
+      if hour.present?
+        key = hour[0].to_sym
+        raw_value = hour[1]
+        value = eval(raw_value) if raw_value.include?('{') and raw_value.include?('}')
+        new_hours[key] = value
+      end
+      new_hours
+    end
+  end
+
+  def breaks
+    super.inject({}) do |new_breaks, breakk|
+      if breakk.present?
+        key = Date.parse(breakk[0])
+        raw_value = breakk[1]
+        value = eval(raw_value) if raw_value.include?('{') and raw_value.include?('}')
+        new_breaks[key] = value
+      end
+      new_breaks
+    end
+  end
+
+  def biz
+    Biz.configure do |config|
+      config.hours = self.hours
+      config.breaks = self.breaks
+      config.holidays = self.holidays
+      config.time_zone = self.timezone
+    end
+  end
+end
--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,4 +1,5 @@
 class Schedule < ApplicationRecord
+  prepend HasBizConcern
   validates_presence_of :timezone, :hours, :schedulable
   belongs_to :schedulable, polymorphic: true
 end
