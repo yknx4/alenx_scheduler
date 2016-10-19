e8ddd376bb0a7eaa2808680bdad71d8353102cc2
Ale Figueroa <yknx.4.b@gmail.com>
2016-10-18 22:00:52 -0500

    Ensure hstore works with apartment

--- db/migrate/20161018031327_create_schedules.rb
+++ db/migrate/20161018031327_create_schedules.rb
@@ -1,6 +1,6 @@
 class CreateSchedules < ActiveRecord::Migration[5.0]
   def change
-    # enable_extension 'hstore' unless extension_enabled?('hstore')
+    enable_extension 'hstore' unless extension_enabled?('hstore')
     create_table :schedules do |t|
       t.string :timezone, null: false, default: 'America/Mexico_City'
       t.string :holidays, array: true, default: []
