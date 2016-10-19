8593fffb33cc4719dd70edaf7014e9c627f57bdb
Jorge Figueroa <yknx.4.b@gmail.com>
2016-10-19 14:11:46 -0500

    validates appointment date

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,5 +1,14 @@
 class Appointment < ApplicationRecord
   validates_presence_of :user, :provider, :start_time, :end_time
+  validate :star_date_cannot_be_later_than_end_time
+  
   belongs_to :provider, class_name: 'User'
   belongs_to :user
+
+  private
+  def star_date_cannot_be_later_than_end_time
+    if start_time > end_time
+      errors.add :start_time, "can't be later than end time"
+    end
+  end
 end
--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -11,5 +11,10 @@ RSpec.describe Appointment, type: :model do
       a = build(:appointment, user: nil)
       expect(a.valid?).to be_falsey
     end
+
+    it 'should be invalid when the start time is greater than end time' do
+      a = build(:appointment, start_time: 15.minutes.from_now, end_time: Time.now)
+      expect(a.valid?).to be_falsey
+    end
   end
 end
