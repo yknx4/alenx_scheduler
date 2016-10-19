fd1cafe55db2137e02112226754e739df7e95e07
Ale Figueroa <yknx.4.b@gmail.com>
2016-10-18 23:01:02 -0500

    Have a schedule in each organization

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,7 +2,20 @@ class Organization < ApplicationRecord
   belongs_to :schedule
   belongs_to :tenant
   validate :only_one_organization
+  validates_presence_of :schedule
+
+  after_initialize :initialize_schedule
+  after_create :save_schedule
+
   private
+  def save_schedule
+    schedule.save unless schedule.persisted?
+  end
+
+  def initialize_schedule
+    self.schedule ||= Schedule.new
+  end
+
   def only_one_organization
     if Organization.count >= 1
       errors[:base] << 'can only exists once per tenant'
--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#valid' do
+    it 'should be invalid without a schedule' do
+      o = build(:organization, schedule: nil)
+      expect(o.valid?).to be_falsey
+      expect(o.errors[:schedule].empty?).to be_falsey
+    end
+  end
+  describe '#new' do
+    it 'should create a schedule if not present' do
+      o = Organization.create name: 'test'
+      expect(o.schedule.present?).to be_truthy
+    end
+  end
 end
