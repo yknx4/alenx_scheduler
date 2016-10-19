d492b8e49d2f6655936ba6b2ff8763f3a0f4e00f
Jorge Figueroa <yknx.4.b@gmail.com>
2016-10-19 15:31:32 -0500

    Validates appointments never overlaps

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,9 +1,27 @@
 class Appointment < ApplicationRecord
   validates_presence_of :user, :provider, :start_time, :end_time
   validate :star_date_cannot_be_later_than_end_time
+  validate :should_not_overlap
   
   belongs_to :provider, class_name: 'User'
   belongs_to :user
+
+  private
+  def should_not_overlap
+    if start_time and end_time and user and provider
+      service = AppointmentService.new provider: self.provider
+      overlaps = service.get_appointments(start_time, end_time).present?
+      if overlaps
+        errors.add :provider, "isn't available at that time"
+      end
+      service.provider = nil
+      service.user = user
+      overlaps = service.get_appointments(start_time, end_time).present?
+      if overlaps
+        errors.add :user, "isn't available at that time"
+      end
+    end
+  end
 
   private
   def star_date_cannot_be_later_than_end_time
--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -16,5 +16,19 @@ RSpec.describe Appointment, type: :model do
       a = build(:appointment, start_time: 15.minutes.from_now, end_time: Time.now)
       expect(a.valid?).to be_falsey
     end
+
+    it 'should be invalid when time overlaps to an existing provider appointment' do
+      appointment = create(:appointment)
+      invalid_appointment = build(:appointment, provider: appointment.provider, start_time: appointment.start_time + 5.minutes, end_time: appointment.end_time + 1.hour)
+      expect(invalid_appointment.valid?).to be_falsey
+      expect(invalid_appointment.errors[:provider].present?).to be_truthy
+    end
+
+    it 'should be invalid when time overlaps to an existing user appointment' do
+      appointment = create(:appointment)
+      invalid_appointment = build(:appointment, user: appointment.user, start_time: appointment.start_time + 5.minutes, end_time: appointment.end_time + 1.hour)
+      expect(invalid_appointment.valid?).to be_falsey
+      expect(invalid_appointment.errors[:user].present?).to be_truthy
+    end
   end
 end
