6e32b166004ee916f6352e9e5de84ed2dcfd3af4
Jorge Figueroa <yknx.4.b@gmail.com>
2016-10-19 17:32:22 -0500

    Use default_tenant on Role spec

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Role, type: :model do
+  include_context 'default_tenant'
+
   describe '#roles' do
     let!(:user) {create(:user)}
     let!(:admin) {create(:admin)}
