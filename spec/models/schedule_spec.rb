14e094f90c1e721a9d33f54af26e2e63baa4c9a2
Ale Figueroa <yknx.4.b@gmail.com>
2016-10-18 22:53:32 -0500

    Update schedule spec

--- spec/models/schedule_spec.rb
+++ spec/models/schedule_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Schedule, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#new' do
+    it 'should have default hours' do
+      schedule = Schedule.new
+      expect(schedule.hours.present?).to be_truthy
+    end
+  end
 end
