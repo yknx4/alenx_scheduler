94341e80f17c26120ee58911db9f50fc4f33c0e4
Ale Figueroa <yknx.4.b@gmail.com>
2016-10-22 09:58:52 -0500

    Update Appointments Helper

--- spec/support/helpers/appointment_helper.rb
+++ spec/support/helpers/appointment_helper.rb
@@ -1,9 +1,20 @@
 module AppointmentHelper
   def random_appointments(max = 10)
     Array.new(rand(max)) do
-      create(:appointment,
-             start_time: Time.current.middle_of_day - rand(60).minutes,
-             end_time: Time.current.middle_of_day + rand(60).minutes)
+      create(:appointment)
     end
+  end
+
+  def sequencial_appointment(base_time = nil)
+    @sequence ||= 0
+    @sequence += 1
+    base_time ||= Time.current.beginning_of_day
+
+    start_time = base_time + (@sequence * 16).minutes
+    end_time = base_time + 15.minutes + (@sequence * 16).minutes
+
+    @sequence += 1
+
+    create :appointment, start_time: start_time, end_time: end_time
   end
 end
