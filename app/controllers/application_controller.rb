3323abb6774f289c68476785bc3ab19609c4bde4
Jorge Figueroa <yknx.4.b@gmail.com>
2016-10-25 16:49:11 -0500

    Fix Rubocop issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   include ApplicationHelper
   include DeviseTokenAuth::Concerns::SetUserByToken
-  protect_from_forgery with: :null_session
+  protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_user_as_admin, if: :without_tenant?, only: [:create]
 
