8227e30e48d5e29943aac90893d9c30fedeb3ab5
Ale Figueroa <yknx.4.b@gmail.com>
2016-10-25 23:00:37 -0500

    Inherit API UserController from ResourceController

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,6 @@
 module Api
   module V1
     class UsersController < ResourceController
-      def index
-        options = {}
-        serialization = ActiveModelSerializers::SerializableResource.new(User.all, options)
-        render json: serialization.as_json
-      end
     end
   end
 end
--- spec/controllers/api/v1/user_controller_spec.rb
+++ spec/controllers/api/v1/user_controller_spec.rb
@@ -2,10 +2,20 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::UsersController, type: :controller do
   include_context 'default_tenant'
+  let(:admin) { create :admin }
+  let(:token) { lambda { admin.create_new_auth_token } }
+
   describe '#index' do
     it 'should show all users' do
-      rand(10).times { create :user }
+      rand(20).times { create :user }
+      request.headers.merge! token.call
       get :index
+      body = JSON.parse(response.body)
+
+      expect(response).to have_http_status(:ok)
+      expect(body).to have_key 'data'
+      expect(body['data'].count).to be > 0
+      expect(body['data'].count).to be <= User.count
     end
   end
 end
